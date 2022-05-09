e881622ffbd011c44825d339d92f36c91e922d21
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-05-09 18:53:02 +0200

    :sparkles:| [ADD] Base program of raylib

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "raylib.h"
+#include <unistd.h>
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic screen manager");
+    CloseWindow();
+    return 0;
+}
