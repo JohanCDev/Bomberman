66d419383a6ed8664176600ff92749c89bb1e507
nlbxyz <nathan.le-bigot@epitech.eu>
2022-05-12 14:14:48 +0200

    [DEBUG] test action windows extern C

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,14 @@
     #include <unistd.h>
 #endif
 
-int main(void)
-{
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+extern "C" {
+    int main(void)
+    {
+        const int screenWidth = 800;
+        const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic screen manager");
-    CloseWindow();
-    return 0;
+        InitWindow(screenWidth, screenHeight, "raylib [core] example - basic screen manager");
+        CloseWindow();
+        return 0;
+    }
 }
