d3eaa50f0979b3370bb8094c3c0051dc3cea84be
Johan Chrillesen <johan@chrillesen.net>
2022-05-16 14:50:28 +0200

    [ADD] pollEvent declaration

--- src/ecs/system/System.hpp
+++ src/ecs/system/System.hpp
@@ -8,16 +8,20 @@
 #ifndef SYSTEM_HPP_
 #define SYSTEM_HPP_
 
-namespace ecs {
+#include "events.hpp"
 
+namespace ecs
+{
     class System {
-        public:
-            System();
-            ~System();
-        protected:
-        private:
+      public:
+        System();
+        ~System();
+        static bool pollEvent(indie::Event &event);
+
+      protected:
+      private:
     };
 
-}
+} // namespace ecs
 
 #endif /* !SYSTEM_HPP_ */
