57114a933fbc3e400425a921b4a249310dae82fc
Johan Chrillesen <johan@chrillesen.net>
2022-05-16 16:52:13 +0200

    [REF] Raylib.hpp

--- src/ecs/raylib/Raylib.hpp
+++ src/ecs/raylib/Raylib.hpp
@@ -8,35 +8,38 @@
 #ifndef RAYLIB_HPP_
 #define RAYLIB_HPP_
 
-#include <raylib.h>
 #include <cstdio>
+#include <raylib.h>
 #include <string>
+#include "events.hpp"
+#include "RColor.hpp"
 
 class Raylib {
-    public:
-        Raylib();
-        ~Raylib();
+  public:
+    Raylib();
+    ~Raylib();
 
-        // Setup function
-        static void initWindow(size_t width = 1920, size_t heigth = 1080, const std::string title = "Window", bool resizable = true, int baseFps = 60);
-        static void destroyWindow();
-        
-        // Event function
-        static bool isKeyPressed(KeyboardKey key);
-        
-        // Window function
-        static void beginDrawing();
-        static void endDrawing();
-        static void clearBackground();
-        static bool windowShouldClose();
+    // Setup function
+    static void initWindow(size_t width = 1920, size_t heigth = 1080, const std::string title = "Window",
+        bool resizable = true, int baseFps = 60);
+    static void destroyWindow();
 
-        // Drawing function
-        static void drawText(const std::string text, int posX, int posY, int fontSize, Color color);
-        static void drawCircle(int posX, int posY, float radius, Color color);
-        static void drawRectangle(int posX, int posY, int width, int height, Color color);
+    // Event function
+    static bool isKeyPressed(KeyboardKey key);
 
-    protected:
-    private:
+    // Window function
+    static void beginDrawing();
+    static void endDrawing();
+    static void clearBackground();
+    static bool windowShouldClose();
+
+    // Drawing function
+    static void drawText(const std::string text, int posX, int posY, int fontSize, rColor color);
+    static void drawCircle(int posX, int posY, float radius, rColor color);
+    static void drawRectangle(int posX, int posY, int width, int height, rColor color);
+
+  protected:
+  private:
 };
 
 #endif /* !RAYLIB_HPP_ */
