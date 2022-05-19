aa16ba61fedf1542b8f2a9b26a2689c373b87dd8
Johan Chrillesen <johan@chrillesen.net>
2022-05-19 17:03:17 +0200

    [REM] extern C in Ralib encapsulation

--- src/ecs/raylib/Raylib.cpp
+++ src/ecs/raylib/Raylib.cpp
@@ -15,60 +15,57 @@ Raylib::~Raylib()
 {
 }
 
-extern "C"
+void Raylib::initWindow(size_t width, size_t heigth, const std::string title, bool resizable, int baseFps)
 {
-    void Raylib::initWindow(size_t width, size_t heigth, const std::string title, bool resizable, int baseFps)
-    {
-        if (resizable)
-            SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
-        else
-            SetConfigFlags(FLAG_VSYNC_HINT);
-        InitWindow(width, heigth, title.c_str());
-        SetTargetFPS(baseFps);
-    }
+    if (resizable)
+        SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+    else
+        SetConfigFlags(FLAG_VSYNC_HINT);
+    InitWindow(width, heigth, title.c_str());
+    SetTargetFPS(baseFps);
+}
 
-    void Raylib::destroyWindow()
-    {
-        CloseWindow();
-    }
+void Raylib::destroyWindow()
+{
+    CloseWindow();
+}
 
-    bool Raylib::windowShouldClose()
-    {
-        return (WindowShouldClose());
-    }
+bool Raylib::windowShouldClose()
+{
+    return (WindowShouldClose());
+}
 
-    void Raylib::beginDrawing()
-    {
-        BeginDrawing();
-    }
+void Raylib::beginDrawing()
+{
+    BeginDrawing();
+}
 
-    void Raylib::endDrawing()
-    {
-        EndDrawing();
-    }
+void Raylib::endDrawing()
+{
+    EndDrawing();
+}
 
-    bool Raylib::isKeyPressed(KeyboardKey key)
-    {
-        return (IsKeyPressed(key));
-    }
+bool Raylib::isKeyPressed(KeyboardKey key)
+{
+    return (IsKeyPressed(key));
+}
 
-    void Raylib::clearBackground()
-    {
-        ClearBackground((Color){245, 245, 245, 255});
-    }
+void Raylib::clearBackground()
+{
+    ClearBackground((Color){245, 245, 245, 255});
+}
 
-    void Raylib::drawText(const std::string text, int posX, int posY, int fontSize, rColor color)
-    {
-        DrawText(text.c_str(), posX, posY, fontSize, (Color){color.r, color.g, color.b, color.a});
-    }
+void Raylib::drawText(const std::string text, int posX, int posY, int fontSize, rColor color)
+{
+    DrawText(text.c_str(), posX, posY, fontSize, (Color){color.r, color.g, color.b, color.a});
+}
 
-    void Raylib::drawCircle(int posX, int posY, float radius, rColor color)
-    {
-        DrawCircle(posX, posY, radius, (Color){color.r, color.g, color.b, color.a});
-    }
+void Raylib::drawCircle(int posX, int posY, float radius, rColor color)
+{
+    DrawCircle(posX, posY, radius, (Color){color.r, color.g, color.b, color.a});
+}
 
-    void Raylib::drawRectangle(int posX, int posY, int width, int height, rColor color)
-    {
-        DrawRectangle(posX, posY, width, height, (Color){color.r, color.g, color.b, color.a});
-    }
+void Raylib::drawRectangle(int posX, int posY, int width, int height, rColor color)
+{
+    DrawRectangle(posX, posY, width, height, (Color){color.r, color.g, color.b, color.a});
 }
