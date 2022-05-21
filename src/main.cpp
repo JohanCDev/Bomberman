9a9ebcaf9e17c6c69cb2bc147bee6c586b064f4b
simlf <simon.le-fourn@epitech.eu>
2022-05-21 15:44:26 +0200

    [UPD] Use new classes in main (e.g. Window, Event and Draw)

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,31 @@
 #include "ecs/component/Component.hpp"
 #include "ecs/entity/Entity.hpp"
 #include "ecs/raylib/Raylib.hpp"
+#include "ecs/raylib/Window.hpp"
+#include "ecs/raylib/Draw.hpp"
+#include "ecs/raylib/Event.hpp"
 #include "RColor.hpp"
 #include <iostream>
 
 int main(void)
 {
     ecs::Entity entity;
+    Raylib::Window window;
+    Raylib::Draw draw;
+    Raylib::Event event;
 
-    Raylib::initWindow(1920, 1000, "Indie Studio", true);
-    while (!Raylib::windowShouldClose()) {
-        if (Raylib::isKeyPressed(KEY_SPACE)) {
+    window.initWindow(1920, 1000, "Indie Studio", true);
+    while (!window.windowShouldClose()) {
+        if (event.isKeyPressed(KEY_SPACE)) {
             std::cout << "a" << std::endl;
         }
-        Raylib::beginDrawing();
-        Raylib::clearBackground();
-        Raylib::drawText("L'INDIE STUDIO EST FINIIIIIII", 100, 100, 50, BLACK);
-        Raylib::drawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2,  10.0, RED);
-        Raylib::endDrawing();
+        window.beginDrawing();
+        window.clearBackground();
+        draw.drawText("L'INDIE STUDIO EST FINIIIIIII", 100, 100, 50, BLACK);
+        draw.drawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2,  10.0, RED);
+        window.endDrawing();
     }
-    Raylib::destroyWindow();
+    window.destroyWindow();
     entity.addComponent<ecs::Position>(100.0, 100.0);
     entity.addComponent<ecs::Movement>(30.0, 30.0);
     entity.getPosition();
