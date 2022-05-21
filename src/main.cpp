25e6bd3bfccd403272079576853b3f0d157c325b
Victor Harri-Chal <victor.harri-chal@epitech.eu>
2022-05-21 16:31:33 +0200

    [ADD] Map generation, random empty spaces and random bonus placements.

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,30 @@
 /*
 ** EPITECH PROJECT, 2022
-** main
+** MapGenerator
 ** File description:
 ** main
 */
 
-#include "ecs/component/Component.hpp"
-#include "ecs/entity/Entity.hpp"
-#include "ecs/raylib/Raylib.hpp"
-#include "RColor.hpp"
-#include <iostream>
+#include "MapGenerator.hpp"
+
+void print_vector(std::vector<std::vector<char>> const &map)
+{
+    for (int i = 0; i < 21; i++) {
+       for (int j = 0; j < 21; j++) {
+           std::cout << map[i][j];
+       }
+       std::cout << std::endl;
+    }
+}
 
 int main(void)
-{
-    ecs::Entity entity;
-
-    Raylib::initWindow(1920, 1000, "Indie Studio", true);
-    while (!Raylib::windowShouldClose()) {
-        if (Raylib::isKeyPressed(KEY_SPACE)) {
-            std::cout << "a" << std::endl;
-        }
-        Raylib::beginDrawing();
-        Raylib::clearBackground();
-        Raylib::drawText("L'INDIE STUDIO EST FINIIIIIII", 100, 100, 50, BLACK);
-        Raylib::drawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2,  10.0, RED);
-        Raylib::endDrawing();
-    }
-    Raylib::destroyWindow();
-    entity.addComponent<ecs::Position>(100.0, 100.0);
-    entity.addComponent<ecs::Movement>(30.0, 30.0);
-    entity.getPosition();
-    entity.getMovement();
-    // entity.getCircleRadius();
-    return (0);
+{  
+    MapGenerator map;
+    map.createWall();
+    // map.doSpawnArea();
+    // map.destroyRandomWall();
+    // map.assignPercentagePowerUp();
+    // map.callPlaceRandomPowerUp();
+    print_vector(map.getMap());
+    return 0;
 }
