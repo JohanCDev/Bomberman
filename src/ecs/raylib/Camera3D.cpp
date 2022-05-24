2ddb0510dde2de0940ac60a0d9268857e3dcf645
simlf <simon.le-fourn@epitech.eu>
2022-05-25 00:18:53 +0200

    [UPD] Add getters and setters for Camera3D class

--- src/ecs/raylib/Camera3D.cpp
+++ src/ecs/raylib/Camera3D.cpp
@@ -7,4 +7,72 @@
 
 #include "Camera3D.hpp"
 
-Raylib::Camera3D::Camera3D(::Vector3 position, ::Vector3 target, ::Vector3 up, float fovy, int type) : _position(position), _target(target),  _up(up),  _fovy(fovy),  _type(type) {}
+Raylib::Camera3D::Camera3D(::Vector3 position, ::Vector3 target, ::Vector3 up, float fovy, int projection)
+{
+    _camera.position = position;
+    _camera.target = target;
+    _camera.up = up;
+    _camera.fovy = fovy;
+    _camera.projection = projection;
+}
+
+void Raylib::Camera3D::setPosition(float newX, float newY, float newZ)
+{
+    _camera.position.x = newX;
+    _camera.position.y = newY;
+    _camera.position.z = newZ;
+}
+
+::Vector3 Raylib::Camera3D::getPosition() const
+{
+    return _camera.position;
+}
+
+void Raylib::Camera3D::setTarget(float newX, float newY, float newZ)
+{
+    _camera.target.x = newX;
+    _camera.target.y = newY;
+    _camera.target.z = newZ;
+}
+
+::Vector3 Raylib::Camera3D::getTarget() const
+{
+    return _camera.target;
+}
+
+void Raylib::Camera3D::setUp(float newX, float newY, float newZ)
+{
+    _camera.up.x = newX;
+    _camera.up.y = newY;
+    _camera.up.z = newZ;
+}
+
+::Vector3 Raylib::Camera3D::getUp() const
+{
+    return _camera.up;
+}
+
+void Raylib::Camera3D::setFovy(float newFovy)
+{
+    _camera.fovy = newFovy;
+}
+
+float Raylib::Camera3D::getFovy() const
+{
+    return _camera.fovy;
+}
+
+void Raylib::Camera3D::setProjection(int newProjection)
+{
+    _camera.projection = newProjection;
+}
+
+int Raylib::Camera3D::getProjection() const
+{
+    return _camera.projection;
+}
+
+// void Raylib::Camera3D::BeginMode()
+// {
+//     ::BeginMode3D(_camera);
+// }
--- src/ecs/raylib/Camera3D.hpp
+++ src/ecs/raylib/Camera3D.hpp
@@ -12,16 +12,90 @@
 
 namespace Raylib {
     class Camera3D {
+
+        typedef struct RCamera3D {
+            ::Vector3 position;       // Camera position
+            ::Vector3 target;         // Camera target it looks-at
+            ::Vector3 up;             // Camera up vector (rotation over its axis)
+            float fovy;             // Camera field-of-view apperture in Y (degrees) in perspective, used as near plane width in orthographic
+            int projection;         // Camera projection: CAMERA_PERSPECTIVE or CAMERA_ORTHOGRAPHIC
+        } RCamera3D;
+
         public:
-            Camera3D(::Vector3 position, ::Vector3 target, ::Vector3 up, float fovy, int type);
-            ~Camera3D();
+            /**
+            * Create a new Camera3D.
+            *
+            * @param position Camera position
+            * @param target Camera target it looks-at
+            * @param up Camera up vector (rotation over its axis)
+            * @param fovy Camera field-of-view apperture in Y (degrees) in perspective, used as near plane width in orthographic
+            * @param projection Camera projection: CAMERA_PERSPECTIVE or CAMERA_ORTHOGRAPHIC
+            */
+            Camera3D(::Vector3 position, ::Vector3 target, ::Vector3 up, float fovy, int projection);
+            ~Camera3D() = default;
+
+            /**
+            * @brief Set camera position
+            */
+            void setPosition(float newX, float newY, float newZ);
+
+            /**
+            * @brief Get camera position
+            */
+            ::Vector3 getPosition() const;
+
+            /**
+            * @brief Set camera target
+            */
+            void setTarget(float newX, float newY, float newZ);
+
+            /**
+            * @brief Get camera target
+            */
+            ::Vector3 getTarget() const;
+
+            /**
+            * @brief Set camera up
+            */
+            void setUp(float newX, float newY, float newZ);
+
+            /**
+            * @brief Get camera up
+            */
+            ::Vector3 getUp() const;
+
+            /**
+            * @brief Set camera fovy
+            */
+            void setFovy(float newFovy);
+
+            /**
+            * @brief Get camera fovy
+            */
+            float getFovy() const;
+
+            /**
+            * @brief Set camera projection
+            */
+            void setProjection(int newProjection);
+
+            /**
+            * @brief Get camera projection
+            */
+            int getProjection() const;
+
+            /**
+             * @brief Initializes 3D mode with custom camera (3D)
+            */
+            void BeginMode();
+
+            /**
+            * @brief Ends 3D mode and returns to default 2D orthographic mode
+            */
+            Camera3D& EndMode();
 
         private:
-            ::Vector3 _position;
-            ::Vector3 _target;
-            ::Vector3 _up;
-            float _fovy;
-            int _type;
+            RCamera3D _camera;
     };
 }
 
