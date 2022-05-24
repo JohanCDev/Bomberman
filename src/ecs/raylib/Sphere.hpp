24dd42ad6c0d0fe7735d861e96365c63f2c50d95
simlf <simon.le-fourn@epitech.eu>
2022-05-24 23:12:31 +0200

    [ADD] Sphere class from Raylib functions

---
+++ src/ecs/raylib/Sphere.hpp
@@ -0,0 +1,71 @@
+/*
+** EPITECH PROJECT, 2022
+** Bomberman
+** File description:
+** Sphere
+*/
+
+#ifndef SPHERE_HPP_
+#define SPHERE_HPP_
+
+#include "Raylib.hpp"
+
+namespace Raylib {
+    class Sphere {
+        public:
+            Sphere(::Vector3 centerPos, float radius, ::Color color);
+            ~Sphere() = default;
+
+            /**
+            * @brief Get cube center position
+            */
+            ::Vector3 getPosition() const;
+
+            /**
+            * @brief Set cube center position
+            */
+            void setPosition(float newX, float newY, float newZ);
+
+            /**
+            * @brief Get cube radius
+            */
+            float getRadius() const;
+
+            /**
+            * @brief Set cube radius
+            */
+            void setRadius(float radius);
+
+            /**
+            * @brief Get cube color
+            */
+            ::Color getColor() const;
+
+            /**
+            * @brief Set cube color
+            */
+            void setColor(::Color color);
+
+            /**
+            * @brief Draw sphere
+            */
+            void draw();
+
+            /**
+            * @brief Draw sphere wire
+            */
+            void drawWires(int rings, int slices`);
+
+            /**
+            * @brief Check collision spheres
+            */
+            bool checkCollisionSpheres(const ::Vector3& sphere2, float radius2);
+
+        private:
+            ::Vector3 _centerPos;
+            float _radius;
+            ::Color _color;
+    };
+}
+
+#endif /* !SPHERE_HPP_ */
