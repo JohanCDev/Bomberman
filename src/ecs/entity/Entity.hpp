120d25a4119dcdbe8de7c1070ba058e73f08065c
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-05-25 11:36:39 +0200

    [ADD] Entity documentation

--- src/ecs/entity/Entity.hpp
+++ src/ecs/entity/Entity.hpp
@@ -16,18 +16,42 @@
 #include <memory>
 
 namespace ecs {
+    /**
+     * @brief Entity base class
+     * 
+     */
     class Entity {
         public:
+            /**
+             * @brief Construct a new Entity object
+             */
             Entity();
+            /**
+             * @brief Destroy the Entity object
+             */
             ~Entity();
 
             template<typename T, typename... Args>
+            /**
+             * @brief Add a component to the entity
+             * 
+             * @param args all components to add
+             */
             void addComponent(Args... args) {
                 T *newCompo(new T(std::forward<Args>(args)...));
                 this->_componentMap.emplace(typeid(T).name(), newCompo);
             }
+            /**
+             * @brief Get the Position object of the entity
+             */
             void getPosition();
+            /**
+             * @brief Get the Movement object of the entity
+             */
             void getMovement();
+            /**
+             * @brief Get the CircleRadius object of the entity
+             */
             void getCircleRadius();
 
         protected:
