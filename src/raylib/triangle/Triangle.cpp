105fb265640b00ad8c4e3f4867fe804e6338c67c
simlf <simon.le-fourn@epitech.eu>
2022-05-26 20:04:19 +0200

    [UPD] Add getters and setters for Triangle

--- src/raylib/triangle/Triangle.cpp
+++ src/raylib/triangle/Triangle.cpp
@@ -9,6 +9,44 @@
 
 indie::raylib::Triangle::Triangle(::Vector2 v1, ::Vector2 v2, ::Vector2 v3, ::Color color) : _v1(v1), _v2(v2), _v3(v3) {}
 
+::Vector2 indie::raylib::Triangle::getVec1() const
+{
+    return _v1;
+}
+
+void indie::raylib::Triangle::setVec1(::Vector2 v1)
+{
+    _v1 = v1;
+}
+
+::Vector2 indie::raylib::Triangle::getVec2() const
+{
+    return _v2;
+}
+
+void indie::raylib::Triangle::setVec2(::Vector2 v2)
+{
+    _v2 = v2;
+}
+
+::Vector2 indie::raylib::Triangle::getVec3() const
+{
+    return _v3;
+}
+
+void indie::raylib::Triangle::setVec3(::Vector2 v3)
+{
+    _v3 = v3;
+}
+
+::Color indie::raylib::Triangle::getColor() const {
+    return _color;
+}
+
+void indie::raylib::Triangle::setColor(::Color color) {
+    _color = color;
+}
+
 void indie::raylib::Triangle::draw()
 {
     ::DrawTriangle(_v1, _v2, _v3, _color);
--- src/raylib/triangle/Triangle.hpp
+++ src/raylib/triangle/Triangle.hpp
@@ -18,6 +18,46 @@ namespace indie {
                 ~Triangle() = default;
 
                 /**
+                * @brief Get triangle v1
+                */
+                ::Vector2 getVec1() const;
+
+                /**
+                * @brief Set triangle v1
+                */
+                void setVec1(::Vector2 v1);
+
+                /**
+                * @brief Get triangle v2
+                */
+                ::Vector2 getVec2() const;
+
+                /**
+                * @brief Set triangle v2
+                */
+                void setVec2(::Vector2 v2);
+
+                /**
+                * @brief Get triangle v3
+                */
+                ::Vector2 getVec3() const;
+
+                /**
+                * @brief Set triangle v3
+                */
+                void setVec3(::Vector2 v3);
+
+                /**
+                * @brief Get triangle color
+                */
+                ::Color getColor() const;
+
+                /**
+                * @brief Set triangle color
+                */
+                void setColor(::Color color);
+
+                /**
                 * @brief Draw a color-filled triangle (vertex in counter-clockwise order!)
                 */
                 void draw();
