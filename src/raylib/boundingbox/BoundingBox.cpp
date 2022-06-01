08ea5def53f68e6fe984ef06e6c0b5724800025a
simlf <simon.le-fourn@epitech.eu>
2022-06-01 11:40:17 +0200

    [UPD] Set member functions of BoundingBox class to static

--- src/raylib/boundingbox/BoundingBox.cpp
+++ src/raylib/boundingbox/BoundingBox.cpp
@@ -7,50 +7,21 @@
 
 #include "BoundingBox.hpp"
 
-indie::raylib::BoundingBox::BoundingBox(::Vector3 min, ::Vector3 max, ::Color color) {
-    _boundingBox.min = min;
-    _boundingBox.max = max;
-    _color = color;
-}
-
-::BoundingBox indie::raylib::BoundingBox::getBoundingBox() const
+void indie::raylib::BoundingBox::draw(::BoundingBox boundingBox, ::Color color)
 {
-    return _boundingBox;
+    ::DrawBoundingBox(boundingBox, color);
 }
 
-void indie::raylib::BoundingBox::setBoundingBox(::Vector3 min, ::Vector3 max)
+void indie::raylib::BoundingBox::draw(::Vector3 min, ::Vector3 max, ::Color color)
 {
-    _boundingBox.min = min;
-    _boundingBox.max = max;
+    ::BoundingBox box;
+
+    box.min = min;
+    box.max = max;
+    ::DrawBoundingBox(box, color);
 }
 
-::Color indie::raylib::BoundingBox::getColor() const {
-    return _color;
-}
-
-void indie::raylib::BoundingBox::setColor(::Color color) {
-    _color = color;
-}
-
-void indie::raylib::BoundingBox::draw()
+bool indie::raylib::BoundingBox::checkCollisionBoxes(::BoundingBox box1, ::BoundingBox box2)
 {
-    ::DrawBoundingBox(_boundingBox, _color);
+    ::CheckCollisionBoxes(box1, box2);
 }
-
-void indie::raylib::BoundingBox::draw(::Color color)
-{
-    ::DrawBoundingBox(_boundingBox, color);
-}
-
-bool  indie::raylib::BoundingBox::checkCollisionBoxes(::BoundingBox box2)
-{
-    return ::CheckCollisionBoxes(_boundingBox, box2);
-}
-
-// /**
-// * @brief Check collision between this box and a sphere
-// */
-// bool indie::raylib::BoudingBox::checkCollisionBoxSphere(indie::raylib::Sphere sphere)
-// {
-//     ::CheckCollisionBoxSphere(_box, sphere.getCenterPosition(), sphere.getRadius());
-// }
