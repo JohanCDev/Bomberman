df8ad8fdeb53f9a8065abddbf4fad38d435f8cc7
simlf <simon.le-fourn@epitech.eu>
2022-06-01 14:48:35 +0200

    [UPD] Add gamePad related member functions to Event class

--- src/raylib/event/Event.cpp
+++ src/raylib/event/Event.cpp
@@ -11,3 +11,18 @@ bool indie::raylib::Event::isKeyPressed(KeyboardKey key)
 {
     return (IsKeyPressed(key));
 }
+
+bool indie::raylib::Event::isGamepadAvailable(int gamepad)
+{
+    ::IsGamepadAvailable(gamepad);
+}
+
+int indie::raylib::Event::getGamepadButtonPressed(void)
+{
+    ::GetGamepadButtonPressed();
+}
+
+float indie::raylib::Event::getGamepadAxisMovement(int gamepad, int axis)
+{
+    ::GetGamepadAxisMovement(gamepad, axis);
+}
