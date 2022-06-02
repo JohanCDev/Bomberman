16fe4849f5c7a32cd245ee3f9d52b308bb265da2
simlf <simon.le-fourn@epitech.eu>
2022-06-02 15:22:07 +0200

    [UPD] Set back the non static member functions in BoundingBox

--- src/raylib/boundingbox/BoundingBox.cpp
+++ src/raylib/boundingbox/BoundingBox.cpp
@@ -7,6 +7,41 @@
 
 #include "BoundingBox.hpp"
 
+indie::raylib::BoundingBox::BoundingBox(::Vector3 min, ::Vector3 max, ::Color color) {
+    _boundingBox.min = min;
+    _boundingBox.max = max;
+    _color = color;
+}
+
+::BoundingBox indie::raylib::BoundingBox::getBoundingBox() const
+{
+    return _boundingBox;
+}
+
+void indie::raylib::BoundingBox::setBoundingBox(::Vector3 min, ::Vector3 max)
+{
+    _boundingBox.min = min;
+    _boundingBox.max = max;
+}
+
+::Color indie::raylib::BoundingBox::getColor() const {
+    return _color;
+}
+
+void indie::raylib::BoundingBox::setColor(::Color color) {
+    _color = color;
+}
+
+void indie::raylib::BoundingBox::draw()
+{
+    ::DrawBoundingBox(_boundingBox, _color);
+}
+
+void indie::raylib::BoundingBox::draw(::Color color)
+{
+    ::DrawBoundingBox(_boundingBox, color);
+}
+
 void indie::raylib::BoundingBox::draw(::BoundingBox boundingBox, ::Color color)
 {
     ::DrawBoundingBox(boundingBox, color);
@@ -19,6 +54,11 @@ void indie::raylib::BoundingBox::draw(::Vector3 min, ::Vector3 max, ::Color colo
     box.min = min;
     box.max = max;
     ::DrawBoundingBox(box, color);
+}
+
+bool  indie::raylib::BoundingBox::checkCollisionBoxes(::BoundingBox box2)
+{
+    return ::CheckCollisionBoxes(_boundingBox, box2);
 }
 
 bool indie::raylib::BoundingBox::checkCollisionBoxes(::BoundingBox box1, ::BoundingBox box2)
