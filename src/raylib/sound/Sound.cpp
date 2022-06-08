bbba97ef26858f492b846a7c499701e4aa275bcd
simlf <simon.le-fourn@epitech.eu>
2022-06-08 11:56:51 +0200

    [ADD] Static Member functions for Sound class

--- src/raylib/sound/Sound.cpp
+++ src/raylib/sound/Sound.cpp
@@ -11,24 +11,40 @@ indie::raylib::Sound::Sound(const std::string& fileName) {
     _sound = ::LoadSound(fileName.c_str());
 }
 
-indie::raylib::Sound::~Sound() {
-    ::UnloadSound(_sound);
+void indie::raylib::Sound::play(::Sound sound) {
+    ::PlaySound(sound);
 }
 
 void indie::raylib::Sound::play() {
     ::PlaySound(_sound);
 }
 
+void indie::raylib::Sound::stop(::Sound sound) {
+    ::StopSound(sound);
+}
+
 void indie::raylib::Sound::stop() {
     ::StopSound(_sound);
+}
+
+void indie::raylib::Sound::pause(::Sound sound) {
+    ::PauseSound(sound);
 }
 
 void indie::raylib::Sound::pause() {
     ::PauseSound(_sound);
 }
 
+void indie::raylib::Sound::resume(::Sound sound) {
+    ::ResumeSound(sound);
+}
+
 void indie::raylib::Sound::resume() {
     ::ResumeSound(_sound);
+}
+
+void indie::raylib::Sound::setVolume(::Sound sound, float volume) {
+    ::SetSoundVolume(sound, volume);
 }
 
 void indie::raylib::Sound::setVolume(float volume) {
@@ -37,6 +53,14 @@ void indie::raylib::Sound::setVolume(float volume) {
 
 void indie::raylib::Sound::setPitch(float pitch) {
     ::SetSoundPitch(_sound, pitch);
+}
+
+void indie::raylib::Sound::setPitch(::Sound sound, float pitch) {
+    ::SetSoundPitch(sound, pitch);
+}
+
+bool indie::raylib::Sound::isPlaying(::Sound sound) {
+    return ::IsSoundPlaying(sound);
 }
 
 bool indie::raylib::Sound::isPlaying() const {
