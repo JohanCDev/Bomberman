bb77696092ad5c2483921a0f7ba9d0fda90dbcdc
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-06-08 16:55:05 +0200

    [FOR] Formatting

--- src/gameEvents/GameEvents.cpp
+++ src/gameEvents/GameEvents.cpp
@@ -132,7 +132,8 @@ namespace indie
                 event.controller[controllerId].code = indie::Event::ControllerCode::O_BUTTON;
             else if (indie::raylib::Event::isGamepadButtonPressed(controllerId, indie::Event::ControllerCode::T_BUTTON))
                 event.controller[controllerId].code = indie::Event::ControllerCode::T_BUTTON;
-            else if (indie::raylib::Event::isGamepadButtonPressed(controllerId, indie::Event::ControllerCode::OPTION_BUTTON))
+            else if (indie::raylib::Event::isGamepadButtonPressed(
+                         controllerId, indie::Event::ControllerCode::OPTION_BUTTON))
                 event.controller[controllerId].code = indie::Event::ControllerCode::OPTION_BUTTON;
             else
                 event.controller[controllerId].code = indie::Event::ControllerCode::CONTROLLER_NONE;
