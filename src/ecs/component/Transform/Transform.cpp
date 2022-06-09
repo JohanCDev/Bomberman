fde555557bd8db3b2fec801b5237cece1997c976
Cedric <cedric.corge@epitech.eu>
2022-06-09 15:04:31 +0200

    [ADD] Z position in transform

--- src/ecs/component/Transform/Transform.cpp
+++ src/ecs/component/Transform/Transform.cpp
@@ -12,6 +12,7 @@ indie::ecs::component::Transform::Transform()
 {
     this->_posX = 0.0;
     this->_posY = 0.0;
+    this->_posZ = 0.0;
     this->_speedX = 0.0;
     this->_speedY = 0.0;
 }
@@ -20,6 +21,7 @@ indie::ecs::component::Transform::Transform(float posX, float posY, float speedX
 {
     this->_posX = posX;
     this->_posY = posY;
+    this->_posZ = 0.0;
     this->_speedX = speedX;
     this->_speedY = speedY;
 }
@@ -79,4 +81,14 @@ void indie::ecs::component::Transform::setSpeedY(float speed)
 void indie::ecs::component::Transform::setSpeedX(float speed)
 {
     this->_speedX = speed;
+}
+
+void indie::ecs::component::Transform::setZ(float posZ)
+{
+    this->_posZ = posZ;
+}
+
+float indie::ecs::component::Transform::getZ() const
+{
+    return (this->_posZ);
 }
