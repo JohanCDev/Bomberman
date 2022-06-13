32425b443ac438b8881b3eaf0edc8675ed89a24a
Henri <henri.chauvet@epitech.eu>
2022-06-13 14:55:41 +0200

    [ADD] Doc for Game Option Screen

--- src/screens/game_options/GameOptionsScreen.hpp
+++ src/screens/game_options/GameOptionsScreen.hpp
@@ -25,6 +25,10 @@ namespace indie
     {
         class GameOptionsScreen : public IScreen {
           public:
+            /**
+             * @brief Enum for the Cursor position
+             * 
+             */
             enum cursorPosition {
                 RESUME = 0,
                 MENU = 1,
@@ -34,24 +38,89 @@ namespace indie
                 FPS = 5,
                 EXIT = 6,
             };
+            /**
+             * @brief Construct a new Game Options Screen object
+             * 
+             */
             GameOptionsScreen();
+            /**
+             * @brief Destroy the Game Options Screen object
+             * 
+             */
             ~GameOptionsScreen() = default;
+            /**
+             * @brief Init the Game Option Screen
+             * 
+             */
             void init() override;
+            /**
+             * @brief Draw the Game Option Screen
+             * 
+             */
             void draw() override;
+            /**
+             * @brief Update the Game Option Screen
+             * 
+             */
             void update() override;
+            /**
+             * @brief 
+             * 
+             * @param event the event to be handled
+             * @return int The scene number
+             */
             int handleEvent(indie::Event &event) override;
+            /**
+             * @brief 
+             * 
+             * @param entity the entity to add for the Game Option Screen
+             */
             void addEntity(std::unique_ptr<indie::ecs::entity::Entity> entity) override;
+            /**
+             * @brief 
+             * 
+             * @param entity the entity to remove from the Game Option Screen
+             */
             void addSystem(std::unique_ptr<indie::ecs::system::ISystem> system) override;
-
+            /**
+             * @brief Get the Is Game Options Calling object
+             * 
+             * @return if the game option is called or not
+             */
             bool getIsGameOptionsCalling() const;
+            /**
+             * @brief 
+             * 
+             * @param direction True is UP and False is DOWN
+             * @return int The index for the button
+             */
             int checkCursorPosition(bool direction);
 
           private:
+            /**
+             * @brief Vector of entities for the Game Option Screen
+             * 
+             */
             std::vector<std::unique_ptr<indie::ecs::entity::Entity>> _entities;
+            /**
+             * @brief Vector of systems for the Game Option Screen
+             * 
+             */
             std::vector<std::unique_ptr<indie::ecs::system::ISystem>> _systems;
+            /**
+             * @brief Map of positions of the cursor
+             * 
+             */
             std::map<int, float> _positionsCursor;
+            /**
+             * @brief Index for the cursor position
+             * 
+             */
             int _cursorPosition;
-
+            /**
+             * @brief Is game option called
+             * 
+             */
             bool _isGameOptionsCalling;
         };
     } // namespace menu
