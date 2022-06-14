bf593b3804fd194ff1d06d189406d0670d95dff2
Henri <henri.chauvet@epitech.eu>
2022-06-14 14:13:01 +0200

    [ADD] Doc for Model / Music

--- src/raylib/model/Model.hpp
+++ src/raylib/model/Model.hpp
@@ -28,47 +28,69 @@ namespace indie {
                  * 
                  */
                 ~Model() = default;
-
-                // Model loading/unloading functions
                 /**
                 * @brief Load model from files (meshes and materials)
+                * 
+                * @param fileName Filepath to the model file
                 */
                 ::Model load(const char *fileName);
-
                 /**
                 * @brief Load model from generated mesh (default material)
+                * 
+                * @param mesh Mesh to load
                 */
                 ::Model loadFromMesh(::Mesh mesh);
-
                 /**
                 * @brief Unload model (including meshes) from memory (RAM and/or VRAM)
+                * 
+                * @param model Model to unload
                 */
                 void unload(::Model model);
-
                 /**
                 * @brief Unload model (but not meshes) from memory (RAM and/or VRAM)
+                * 
+                * @param model Model to unload
                 */
                 void unloadKeepMeshes(::Model model);
-
-                // Model drawing functions
                 /**
-                * @brief Draw a model (with texture if set)
-                */
+                 * @brief Draw a model (with texture if set)
+                 * 
+                 * @param model Drawing model
+                 * @param position Drawing position
+                 * @param scale Drawing scale
+                 * @param tint Drawing color
+                 */
                 void draw(::Model model, ::Vector3 position, float scale, ::Color tint);
-
                 /**
-                * @brief Draw a model with extended parameters
-                */
+                 * @brief Draw a model with extended parameters
+                 * 
+                 * @param model Drawing model
+                 * @param position Drawing position
+                 * @param rotationAxis Drawing rotation axis
+                 * @param rotationAngle Drawing rotation angle
+                 * @param scale Drawing scale
+                 * @param tint Drawing color
+                 */
                 void drawEx(::Model model, ::Vector3 position, ::Vector3 rotationAxis, float rotationAngle, ::Vector3 scale, ::Color tint);
-
                 /**
-                * @brief Draw a model wires (with texture if set)
-                */
+                 * @brief Draw a model wires (with texture if set)
+                 * 
+                 * @param model Drawing model
+                 * @param position Drawing position
+                 * @param scale Drawing scale
+                 * @param tint Drawing color
+                 */
                 void drawWires(::Model model, ::Vector3 position, float scale, ::Color tint);
-
                 /**
-                * @brief Draw a model wires (with texture if set) with extended parameters
-                */
+                 * @brief Draw a model wires (with texture if set) with extended parameters
+                 * 
+                 * @param model Drawing model
+                 * @param position Drawing position
+                 * @param rotationAxis Drawing rotation axis
+                 * @param rotationAngle Drawing rotation angle
+                 * @param scale Drawing scale
+                 * @param tint Drawing color
+                 */
                 void drawWiresEx(::Model model, ::Vector3 position, ::Vector3 rotationAxis, float rotationAngle, ::Vector3 scale, ::Color tint);
         };
     }
